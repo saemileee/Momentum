53ef7c92137491b5fd3b693f26301aefe0c97809
saemileee <68241138+saemileee@users.noreply.github.com>
2023-01-14 18:26:32 +0900

    23-01-14 18:25 function learning

--- app.js
+++ app.js
@@ -53,3 +53,72 @@ console.log (itemToBuy);
 
 itemToBuy.checkOutPrice = itemToBuy.price + itemToBuy.deliveryFee;
 console.log (itemToBuy.checkOutPrice);
+
+const plus = 1+1; //변수 plus(변수명)에 1+1을 넣겠다.
+console.log(plus); //콘솔창에 기록해라 (plus)라는 변수를 >> 콘솔창에 "2"가 출력됨
+
+var 넘버 = 1;
+var num2 = 2;
+console.log(num2 - 넘버);
+
+var itemName = "초코송이";
+var itemOption = "바나나맛";
+console.log(itemName + itemOption); //>> 콘솔창에 "초코송이바나나맛"이 출력됨
+console.log(itemName + " " + itemOption)
+
+var lang = "자바스크립트";
+console.log(lang); // 자바스크립트
+var lang = "JavaScripts";
+console.log(lang); // JavaSciprts
+lang = "js";
+console.log(lang); // js
+
+// Function
+
+function sayHello(){
+    console.log("Hello! my name is N"); // sayHello라는 코드를 실행할 때마다 콘솔에 "Hello! my name is N"이라는 텍스트를 출력하라
+};
+// {sayHello라는 기능을 실행할 때 마다 여기 안에 있는 코드가 실행된다는 의미}
+//  (어떻게 기능을 실행시킬거라는 옵션(?)의미)
+// N 자리에 어떤 값을 반복적으로 나타내기 위해 argument 를 실행시켜야함.
+// argument(인수)란 function을 실행하는 동안 어떤 정보를 function에게 보낼 수 있는 방법
+
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is" + nameOfPerson + "and I am " + age); // sayHello라는 코드를 실행할 때마다 콘솔에 "Hello! my name is N"이라는 텍스트를 출력하라
+};
+
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
+//sayHello로 보낸 데이터가 nameOfPerson이라는 변수로 가게된다는 것을 의미
+
+function plusFunc(firstNum, secondNum){
+    console.log(firstNum + ' + '+ secondNum + ' = ' + (firstNum + secondNum));
+};
+
+plusFunc(30, 2);
+
+// *firstNum은 plusFunc이라는 기능 안에서만 존재하고 firNum을 밖에서 호출하면 에러 메세지가 뜸
+
+const player = {
+    name: "saemi",
+    sayHi: function (otherPersonsName){
+        console.log("Hi! " + otherPersonsName + " nice to meet you!");
+    },
+};
+
+console.log(player.name);
+player.sayHi("lynn");
+
+
+const msg = {
+    welcome: function (userName, count){
+        console.log("어서오세요, " + userName + "님! " + count + "번째 방문이네요.");
+    },
+    bye: function (userName){
+        alert("안녕히가세요. " + userName + "님 다음에 또 오세요.");
+    }
+};
+
+msg.welcome("새미", "두");
+msg.bye("성환");
