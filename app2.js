21fcffdd62affd72c6ee6fa17e3fd6c644b89700
saemileee <68241138+saemileee@users.noreply.github.com>
2023-01-18 19:18:08 +0900

    23-01-18 19:18 regular commit

--- app2.js
+++ app2.js
@@ -1,9 +1,9 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-// function onLoginBtnClick(){
-//     const username = loginInput.value;
-//     console.log(username);
-// }
-// loginButton.addEventListener("click", onLoginBtnClick);
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
