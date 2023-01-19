080f94d4fe8fb3ec1b8c375db41c9b19174f5c38
saemileee <68241138+saemileee@users.noreply.github.com>
2023-01-19 12:06:16 +0900

    23-01-19 12:05 regular commit
    userName Submit한 후 userName과 h1 노출하기

--- app2.js
+++ app2.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden"
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
+    if (loginInput.value !==""){
+        const userName = loginInput.value;
+        loginForm.classList.add(HIDDEN_CLASSNAME);
+        greeting.innerText = `Hello ${userName}`; //"Hello " + userName;
+        greeting.classList.remove(HIDDEN_CLASSNAME);
+    } else {alert()}
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+loginForm.addEventListener("submit", onLoginSubmit); 
