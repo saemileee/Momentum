f9bc245b2d57537508d125d7cdd940dd1d4ad7e8
saemileee <68241138+saemileee@users.noreply.github.com>
2023-01-21 23:15:47 +0900

    23-01-21 23:15 regular commit

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+    const hours = String(date.getHours()).padStart(2,"0"); // getHours는 number이기 때문에 string으로 변환해주어야함
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = (`${hours}:${minutes}:${seconds}`)
 }
 
 getClock(); // 페이지가 로드되면 바로 시계가 작동될 수 있게함
