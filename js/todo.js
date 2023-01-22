14a513e3e17bf3f54c344540590d69bf59faa719
saemileee <68241138+saemileee@users.noreply.github.com>
2023-01-23 00:41:14 +0900

    23-01-23 forEach

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,13 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos"
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))}
+
 function deleteToDo(event){
     const li = event.target.parentElement.innerText;
     li.remove();
@@ -19,13 +26,21 @@ function paintToDo(newToDo){
     toDoList.appendChild(li);
 }
 
-
-
 function handleToDoSubmit (event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value=""; //이 시점에 toDoInput value가 비어있더라도 newToDo도 비어있다고 말할 수 없음
+    toDos.push(newToDo);
     paintToDo(newToDo);
+    saveToDos();
 };
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+}
